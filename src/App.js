59c81d7355ea988ce28c7e838b396aafe7693b38
Oriol Carbo <oriol.carbo@gmail.com>
2021-03-17 01:29:17 +0100

    WIP giving design to the calendar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from "./logo.svg";
-import "./App.css";
-import { Calendar } from "./pages/calendar";
+import styles from "./App.module.css";
+import { Calendar } from "./pages/Calendar";
 
 function App() {
   let fullYear = [];
@@ -55,20 +55,8 @@ function App() {
   getFullYear();
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header className={styles.container}>
         <Calendar days={fullYear} />
       </header>
     </div>
--- src/pages/calendar.js
+++ src/pages/calendar.js
@@ -4,7 +4,31 @@ import styles from "./calendar.module.css";
 
 function Calendar(props) {
   console.log(2, props.days);
-  return <div className={styles.test}>Testing</div>;
+  return (
+    <div className={styles.parent}>
+      <div className={styles.box}>
+        January
+        <div style={{ backgroundColor: "red", textAlign: "left" }}>1</div>
+        <div style={{ backgroundColor: "red" }}>2</div>
+        <div style={{ backgroundColor: "red" }}>3</div>
+        <div style={{ backgroundColor: "red" }}>4</div>
+        <div style={{ backgroundColor: "red" }}>5</div>
+        <div style={{ backgroundColor: "red" }}>6</div>
+        <div style={{ backgroundColor: "red" }}>7</div>
+      </div>
+      <div className={styles.box}>February</div>
+      <div className={styles.box}>March</div>
+      <div className={styles.box}>April</div>
+      <div className={styles.box}>May</div>
+      <div className={styles.box}>June</div>
+      <div className={styles.box}>July</div>
+      <div className={styles.box}>August</div>
+      <div className={styles.box}>September</div>
+      <div className={styles.box}>October</div>
+      <div className={styles.box}>November</div>
+      <div className={styles.box}>December</div>
+    </div>
+  );
 }
 
 export { Calendar };
