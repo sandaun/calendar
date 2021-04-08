268af9e6276b9accd07af6128106e8fa7ce860a5
Oriol Carbo <oriol.carbo@gmail.com>
2021-04-08 21:29:02 +0200

    WIP buildMonth and indexes

--- src/components/month/Month.js
+++ src/components/month/Month.js
@@ -3,46 +3,13 @@ import React, { useState, useRef } from "react";
 import styles from "./month.module.css";
 import { Week } from "../Week";
 
-// const buildWeek = (monthDays) => {
-//   const week = [];
-//   const days = [
-//     "Monday",
-//     "Tuesday",
-//     "Wednesday",
-//     "Thursday",
-//     "Friday",
-//     "Saturday",
-//     "Sunday",
-//   ];
-
-//   for (let i = 0; i < monthDays.length; i++) {
-//     for (let j = 0; j < days.length; j++) {
-//       if (monthDays[i].dayNumber === 1) {
-//         if (monthDays[i].dayName === days[j]) {
-//           week.push(<div className={styles.day}>{monthDays[i].dayNumber}</div>);
-//           break;
-//         } else {
-//           week.push(<div className={styles.day}>B</div>);
-//         }
-//       } else if (monthDays[i].dayName === days[j]) {
-//         week.push(<div className={styles.day}>{monthDays[i].dayNumber}</div>);
-//       }
-//     }
-//     if (monthDays[i].dayName === "Sunday") {
-//       // newStateValue = i;
-//       break;
-//     }
-//   }
-//   return week;
-// };
-
 function Month(props) {
   const { monthNames, monthDays } = props;
-  let week = [];
-  const weeks = [];
   let lastDayPosition = 0;
+  let week = [];
+  const totalWeeks = [];
 
-  const buildWeek = (position) => {
+  const buildWeeks = (position) => {
     const days = [
       "Monday",
       "Tuesday",
@@ -52,20 +19,29 @@ function Month(props) {
       "Saturday",
       "Sunday",
     ];
-    console.log(1, monthNames);
     for (let i = position; i < monthDays.length; i++) {
       for (let j = 0; j < days.length; j++) {
         if (monthDays[i].dayNumber === 1) {
           if (monthDays[i].dayName === days[j]) {
             week.push(
-              <div className={styles.day}>{monthDays[i].dayNumber}</div>
+              <div key={i + j} className={styles.day}>
+                {monthDays[i].dayNumber}
+              </div>
             );
             break;
           } else {
-            week.push(<div className={styles.day}>B</div>);
+            week.push(
+              <div key={i + j} className={styles.day}>
+                B
+              </div>
+            );
           }
         } else if (monthDays[i].dayName === days[j]) {
-          week.push(<div className={styles.day}>{monthDays[i].dayNumber}</div>);
+          week.push(
+            <div key={i + j} className={styles.day}>
+              {monthDays[i].dayNumber}
+            </div>
+          );
         }
       }
       if (monthDays[i].dayName === "Sunday" || i === monthDays.length - 1) {
@@ -73,28 +49,28 @@ function Month(props) {
         break;
       }
     }
-    console.log(1111, position);
 
-    weeks.push(week);
+    totalWeeks.push(week);
+
     week = [];
-    // console.log(444, weeks.length);
-    if (weeks.length < 6) {
-      buildWeek(lastDayPosition);
-      console.log(999999999);
+
+    if (totalWeeks.length < 6) {
+      buildWeeks(lastDayPosition);
     }
   };
 
-  buildWeek(lastDayPosition);
+  buildWeeks(lastDayPosition);
+
+  const BuildMonth = () => {
+    return totalWeeks.map((week, index) => {
+      return <Week key={monthNames + index}>{week}</Week>;
+    });
+  };
 
   return (
     <div className={styles.box}>
       {monthNames}
-      <Week>{weeks[0]}</Week>
-      <Week>{weeks[1]}</Week>
-      <Week>{weeks[2]}</Week>
-      <Week>{weeks[3]}</Week>
-      <Week>{weeks[4]}</Week>
-      <Week>{weeks[5]}</Week>
+      <BuildMonth />
     </div>
   );
 }
